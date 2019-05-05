bcb8f246d38763545e017fce8649b9f77e504496
cablespk <solventwaltz@gmail.com>
2019-05-05 10:42:45 -0400

    Fixed unsigned format

--- R/output.R
+++ R/output.R
@@ -75,16 +75,17 @@ output_lwnn_unsigned <- function(file_name) {
 
 	pitchers <- players_all %>%
 		filter(type=="pitcher") %>%
-		select(name,Age,POS,T,IP,ERA,FIP,K.9,BB.9,
-			  OPS_LHB,OPS_RHB,G.GF,STA_SP,STA_RP,
-			  Team_2019,FIP_2019,IP_2019,
-			  FV,ETA,Risk,draft_2018)
+		select(name,Age,POS,T,Team_2019,FIP_2019,
+			  IP_2019,FV,ETA,Risk,draft_2018,
+			  IP,ERA,FIP,K.9,BB.9,OPS_LHB,
+			  OPS_RHB,G.GF,STA_SP,STA_RP
+		)
 
 	hitters <- players_all %>%
 		filter(type=="hitter") %>%
-		select(name,Age,POS,AB,wOBA,OBP,SLG,STL,
-			  OPS_LHP,OPS_RHP,Team_2019,
-			  wOBA_2019,PA_2019,FV,ETA,Risk,draft_2018,
+		select(name,Age,POS,Team_2019,wOBA_2019,PA_2019,
+			  FV,ETA,Risk,draft_2018,
+			  AB,wOBA,OBP,SLG,STL,OPS_LHP,OPS_RHP,
 			  X1B.RN,X1B.ER,X2B.RN,X2B.ER,X3B.RN,X3B.ER,
 			  SS.RN,SS.ER,LF.RN,LF.ER,CF.RN,CF.ER,RF.RN,
 			  RF.ER,OF.ARM,C.RN,C.ER,C.ARM,PB)
@@ -96,9 +97,11 @@ output_lwnn_unsigned <- function(file_name) {
 	writeDataTable(excel_wb, 1, pitchers, startRow = 1, startCol = 1, tableStyle = "TableStyleLight12")
 	writeDataTable(excel_wb, 2, hitters, startRow = 1, startCol = 1, tableStyle = "TableStyleLight14")
 
-	setColWidths(excel_wb, sheet = 1, cols=c(1,15), widths = "auto")
-	setColWidths(excel_wb, sheet = 2, cols=c(1,3,11), widths = "auto")
+	setColWidths(excel_wb, sheet = 1, cols=c(1,5), widths = "auto")
+	setColWidths(excel_wb, sheet = 2, cols=c(1,3,4), widths = "auto")
 
 	saveWorkbook(excel_wb, file_name, overwrite = TRUE)
 
 }
+
+
